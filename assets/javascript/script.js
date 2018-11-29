684420004d6838e8d09a1a4486d66f2548e5e849
Wufasa <mxw373@case.edu>
2018-11-28 18:48:44 -0800

    need to finish while loop for calculating train times

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,6 +30,12 @@ $(document).ready(function(){
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
+        
+    $("#name").text("");
+    $("#destination").text("");
+    $("#startTime").text("");
+    $("#frequency").text("");
+
 
     });
 
@@ -41,15 +47,48 @@ $(document).ready(function(){
         // storing the snapshot.val() in a variable for convenience
         var sv = snapshot.val();
         // Change the HTML to reflect
+        var curH = parseInt(moment().format("h")); 
+        var curMM = parseInt(moment().format("mm"));
+        var a = moment().format("a");
+
+        if (a == "pm"){
+            curH = curH + 12;
+        }
+        //start time = hh:mm military format
+        var startH = sv.startTime.slice(0,2);
+        var startMM = sv.startTime.slice(3,5);
+        var nextTrainTime = -1;
+        var freq = parseInt(sv.frequency);
+//        while (nextTrainTime < 0){
+//            if(startH - curH >= 0){
+//                nextTrainTimeH = startH - curhH;
+//                if(startMM - curMM > 0){
+//                    nextTrainTimeMM = startMM - curMM;
+//                    nextTrainTime = 1;
+//                }
+//                else{
+//                    if(startMM += freq >= 60){
+//                       startH += Math.floor((startMM + freq)/60)
+//                       }
+//                    else{
+//                        startMM += freq;
+//                    }
+//                    
+//                }
+//            }
+//            else{
+//                startH += 1;
+//            }
+//            
+//            
+        }
+        var minAway = nextTrainTimeH * 60 + nextTrainTimeMM;
         var row = $("<tr>");
         row.append($("<td>" + sv.name + "</td>"));
         row.append($("<td>" + sv.destination + "</td>"));
         row.append($("<td>" + sv.frequency + "</td>"));
-        //append months worked, output 1992-03-21
-        //append Next arrival and minutes away using moment.js
-        //append total billed
-        //        var totalBill = monthlyRate * monthsWorked;
-        //        row.append($("<td>" + totalBill  + "</td>"))
+        row.append($("<td>" + nextTrainTimeH + ":" + nextTrainTimeMM + "</td>"));
+        row.append($("<td>") + minAway + "</td>");
         $(".table-body").append(row);
 
         // Handle the errors
