e43cdc711ba0f87fb950e24210515d3de1d7f67f
Wufasa <mxw373@case.edu>
2018-12-01 17:30:21 -0800

    Time functionality works, added try/catch to ensure valid input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,51 +1,73 @@
 // Initialize Firebase
 $(document).ready(function(){
-  var config = {
-    apiKey: "AIzaSyD89edPTjLGdRRPViQiVtJf9VeO0e5WYYE",
-    authDomain: "train-scheduler-ad862.firebaseapp.com",
-    databaseURL: "https://train-scheduler-ad862.firebaseio.com",
-    projectId: "train-scheduler-ad862",
-    storageBucket: "",
-    messagingSenderId: "179598368852"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyD89edPTjLGdRRPViQiVtJf9VeO0e5WYYE",
+        authDomain: "train-scheduler-ad862.firebaseapp.com",
+        databaseURL: "https://train-scheduler-ad862.firebaseio.com",
+        projectId: "train-scheduler-ad862",
+        storageBucket: "",
+        messagingSenderId: "179598368852"
+    };
+    firebase.initializeApp(config);
     // Create a variable to reference the database.
     var database = firebase.database();
-    
+
     //Add train
     $(".submit").on("click", function(event) {
-      event.preventDefault();
+        event.preventDefault();
 
-      // Grabbed values from text boxes
-      name = $("#name").val().trim();
-      destination = $("#destination").val().trim();
-      startTime = $("#startTime").val().trim();
-      frequency = $("#frequency").val().trim();
+        // Grabbed values from text boxes
+        name = $("#name").val().trim();
+        destination = $("#destination").val().trim();
+        frequency = $("#frequency").val().trim();
+        message = document.getElementById("time-error");
+        message.innerHTML = "";
+        //make sure input values is valid
+        try { 
+            startTime = $("#startTime").val().trim();
+            if(startTime == "") throw "empty";
+            startTime = startTime.split(":");
+            for(i=0;i<2;i++){
+                if(isNaN(startTime[i])) throw "not a valid number";
+                if(startTime[i].length > 2) throw "not a valid value";
+                startTime[i] = parseInt(startTime[i]);
+            }
+            if(startTime[0] >= 24) throw "not a valid number";
+            if(startTime[1] > 60) throw "not a valid number";
+            if(isNaN(frequency)) throw "undefined frequency";
+            database.ref().push({
+            name: name,
+            destination: destination,
+            startTime: startTime,
+            frequency: frequency,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        }
+        catch(err) {
+            message.innerHTML = "Input is " + err;
+        }
+       
 
-      // Code for handling the push
-      database.ref().push({
-        name: name,
-        destination: destination,
-        startTime: startTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
         
-    $("#name").text("");
-    $("#destination").text("");
-    $("#startTime").text("");
-    $("#frequency").text("");
+        // Code for handling the push
+
+
+        $("#name").text("");
+        $("#destination").text("");
+        $("#startTime").text("");
+        $("#frequency").text("");
 
 
     });
 
-    
+
     //push 
     //Append to schedule
-    
+
     database.ref().on("child_added", function(snapshot) {
         // storing the snapshot.val() in a variable for convenience
         var sv = snapshot.val();
+        var ampm = "AM";
         // Change the HTML to reflect
         var curH = parseInt(moment().format("h")); 
         var curMM = parseInt(moment().format("mm"));
@@ -55,40 +77,37 @@ $(document).ready(function(){
             curH = curH + 12;
         }
         //start time = hh:mm military format
-        var startH = sv.startTime.slice(0,2);
-        var startMM = sv.startTime.slice(3,5);
+        var startH = sv.startTime[0]
+        var startMM = sv.startTime[1] 
         var nextTrainTime = -1;
         var freq = parseInt(sv.frequency);
-//        while (nextTrainTime < 0){
-//            if(startH - curH >= 0){
-//                nextTrainTimeH = startH - curhH;
-//                if(startMM - curMM > 0){
-//                    nextTrainTimeMM = startMM - curMM;
-//                    nextTrainTime = 1;
-//                }
-//                else{
-//                    if(startMM += freq >= 60){
-//                       startH += Math.floor((startMM + freq)/60)
-//                       }
-//                    else{
-//                        startMM += freq;
-//                    }
-//                    
-//                }
-//            }
-//            else{
-//                startH += 1;
-//            }
-//            
-//            
+        var startT = (startH * 60) + startMM; //Convert to only minutes
+        var curT = (curH * 60) + curMM;
+        while (nextTrainTime < 0){
+            if(startT - curT >= 0){
+                nextTrainTime = startT - curT;
+            }else{
+                startT = startT + freq;
+            }
+
         }
-        var minAway = nextTrainTimeH * 60 + nextTrainTimeMM;
+        nextTrainTimeH = Math.floor(startT/60) % 24;
+        console.log(nextTrainTimeH);
+        if(nextTrainTimeH > 11){ //Change to PM
+            nextTrainTimeH = nextTrainTimeH - 12;
+            console.log("nextTrainh: " + nextTrainTimeH);
+            var ampm = "PM";
+        }
+        nextTrainTimeMM = startT % 60; //edge case
+        if (nextTrainTimeMM == 0){
+            nextTrainTimeMM = "00";
+        }
         var row = $("<tr>");
         row.append($("<td>" + sv.name + "</td>"));
         row.append($("<td>" + sv.destination + "</td>"));
         row.append($("<td>" + sv.frequency + "</td>"));
-        row.append($("<td>" + nextTrainTimeH + ":" + nextTrainTimeMM + "</td>"));
-        row.append($("<td>") + minAway + "</td>");
+        row.append($("<td>" + nextTrainTimeH + ":" + nextTrainTimeMM + ampm + "</td>"));
+        row.append($("<td>" + nextTrainTime + "</td>"));
         $(".table-body").append(row);
 
         // Handle the errors
